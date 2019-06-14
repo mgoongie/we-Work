108df3df660d2892be39abc7d6449f0a0a3c5c43
Gohar Melikyan <goohar.mlkyn@gmail.com>
2019-06-14 20:51:03 +0300

    weWork template done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.logos-slider-box').slick({
+        autoplay: true,
+        slidesToShow: 4,
+        adaptiveHeight: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        cssEase: 'linear',
+        variableWidth: true
+    });
+});
